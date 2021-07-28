8a1829779c06d0fa0c16b642fabebebdaff394e0
CarlosdeAnda <jesparza301@accitesz.com>
2021-07-27 23:18:16 -0500

    Ignore

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "s3cr3t"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin@localhost/Hotel'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/Hotel'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_POOL_SIZE'] = 100
 app.config['UPLOAD_FOLDER'] = "static/uploads/"
